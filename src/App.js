f0448fa0136998337526d2cbb92d896f71e4497d
Clemens Bruesch <clemensbruesch@gmail.com>
2021-09-20 23:40:54 +0200

    header and footer components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1>Howdy!</h1>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+      <h1>An App of Ice And Fire</h1>
+    </header>
+  )
+}
+
+export default Header
